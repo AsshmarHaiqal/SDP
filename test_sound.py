e1409079523350e3301db53d47a388bdfbb60afb
asshmarhaiqal <asshmar03@gmail.com>
2026-03-03 14:42:24 +0000

    add sound actuator

---
+++ test_sound.py
@@ -0,0 +1,11 @@
+from electronic.sound_actuator import SoundActuator
+
+sound = SoundActuator()
+
+sound.verifying_face()
+import time; time.sleep(3)
+
+sound.verified()
+time.sleep(3)
+
+sound.dispensing()
